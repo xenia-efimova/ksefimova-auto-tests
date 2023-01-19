c0f7a2a3bc2a5fd842263c3e57ce58e36f1fe959
Xenia <ks.v.efimova@gmail.com>
2023-01-19 23:48:11 +0300

    Add annotations Feature, Owner; add steps to tests

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,48 +1,66 @@
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class AuthTest {
 
     @BeforeEach
     public void openAuthorizationPage(){
-        // Шаг 1. Открыть страницу https://github.com/
-        open("https://github.com/");
-        // Шаг 2. Нажать на кнопку с текстом Sign in
-        TestPages.mainPage.mainSignInButton().click();
+        step("Открыть страницу https://github.com/", () -> {
+            open("https://github.com/");
+        });
+        step("Нажать на кнопку с текстом Sign in", () -> {
+            TestPages.mainPage.mainSignInButton().click();
+        });
     }
 
     @Test
+    @Owner("xenia-efimova")
     @DisplayName("Успешная авторизация")
+    @Feature("Авторизация")
     public void shouldAuthorizeTest() {
-        // Шаг 3. Заполнить инпуты логина и пароля
-        TestPages.authorizationPage.loginInput().sendKeys("qwerty");
-        TestPages.authorizationPage.passwordInput().sendKeys("qwerty");
-        // Шаг 4. Нажать на кнопку с текстом Sign in
-        TestPages.authorizationPage.singInButton().click();
-        // Шаг 5. Проверка отображения главной страницы после авторизации
-        TestPages.mainPage.mainPageHeader().shouldBe(Condition.visible);
-        // Шаг 6. Кликнуть на кнопку открытия выпадающего списка
-        TestPages.mainPage.buttonOpenDropdown().click();
-        // Шаг 7. Кликнуть на пункт Your profile в выпадающем списке
-        TestPages.mainPage.yourProfileButton().click();
-        // Шаг 8. Проверка открытия страницы профиля
-        TestPages.mainPage.editProfileButton().shouldBe(Condition.visible);
+        step("Заполнить инпуты логина и пароля корректными данными", () -> {
+                    TestPages.authorizationPage.loginInput().sendKeys("qwerty");
+                    TestPages.authorizationPage.passwordInput().sendKeys("qwerty");
+                });
+        step("Нажать на кнопку с текстом Sign in", () -> {
+            TestPages.authorizationPage.singInButton().click();
+        });
+        step("Проверка отображения главной страницы после авторизации", () -> {
+            TestPages.mainPage.mainPageHeader().shouldBe(Condition.visible);
+        });
+        step("Кликнуть на кнопку открытия выпадающего списка", () -> {
+            TestPages.mainPage.buttonOpenDropdown().click();
+        });
+        step("Кликнуть на пункт Your profile в выпадающем списке", () -> {
+            TestPages.mainPage.yourProfileButton().click();
+        });
+        step("Проверка открытия страницы профиля", () -> {
+            TestPages.mainPage.editProfileButton().shouldBe(Condition.visible);
+        });
     }
 
     @Test
+    @Owner("xenia-efimova")
     @DisplayName("Безуспешная авторизация с некорректными данными")
+    @Feature("Авторизация")
     public void shouldNotAuthorizeTest() {
-        // Шаг 3. Заполнить инпуты логина и пароля
-        TestPages.authorizationPage.loginInput().sendKeys("qwerty");
-        TestPages.authorizationPage.passwordInput().sendKeys("qwerty");
-        // Шаг 4. Нажать на кнопку с текстом Sign in
-        TestPages.authorizationPage.singInButton().click();
-        // Шаг 5. Проверка отображения сообщения об ошибке
-        TestPages.authorizationPage.flashAlert().shouldBe(Condition.visible).shouldHave(text("Incorrect username or password"));
+        step("Заполнить инпуты логина и пароля некорректными данными", () -> {
+            TestPages.authorizationPage.loginInput().sendKeys("qwerty");
+            TestPages.authorizationPage.passwordInput().sendKeys("qwerty");
+        });
+        step("Нажать на кнопку с текстом Sign in", () -> {
+            TestPages.authorizationPage.singInButton().click();
+        });
+        step("Проверка отображения сообщения об ошибке", () -> {
+            TestPages.authorizationPage.flashAlert().shouldBe(Condition.visible).shouldHave(text("Incorrect username or password"));
+        });
     }
 }
--- src/test/java/ProfileTests.java
+++ src/test/java/ProfileTests.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,56 +9,77 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class ProfileTests {
 
     @BeforeEach
     public void switchToRepositoriesPage() {
-        // Открыть страницу репозитория "junit-team/junit4"
-        open("https://github.com/junit-team/junit4");
-        // Проверка отображения страницы репозитория "junit-team/junit4"
-        TestPages.repositoryPage.repositoryContainerHeader().shouldHave(text("junit4"));
+        step("Открыть страницу репозитория 'junit-team/junit4'", () -> {
+            open("https://github.com/junit-team/junit4");
+        });
+        step("Проверка отображения страницы репозитория 'junit-team/junit4'", () -> {
+            TestPages.repositoryPage.repositoryContainerHeader().shouldHave(text("junit4"));
+        });
     }
 
     @Test
+    @Owner("xenia-efimova")
     @DisplayName("Переключение на ветку fixtures в репозитории junit-team/junit4")
+    @Feature("Cтраница репозитория")
     public void shouldSwitchToBranchTest() {
-        // Открыть выпадающий список с наименованиями веток
-        TestPages.repositoryPage.dropdownWithNamesOfBranches().click();
-        // Кликнуть на название ветки "fixtures"
-        TestPages.repositoryPage.selectMenuItems().filter(text("fixtures")).first().click();
-        // Проверка отображения страницы ветки fixtures
-        TestPages.repositoryPage.tagOfBranchFixtures().shouldHave(Condition.text("fixtures"));
+        step("Открыть выпадающий список с наименованиями веток", () -> {
+            TestPages.repositoryPage.dropdownWithNamesOfBranches().click();
+        });
+        step("Кликнуть на название ветки 'fixtures'", () -> {
+            TestPages.repositoryPage.selectMenuItems().filter(text("fixtures")).first().click();
+        });
+        step("Проверка отображения страницы ветки fixtures", () -> {
+            TestPages.repositoryPage.tagOfBranchFixtures().shouldHave(Condition.text("fixtures"));
+        });
     }
 
     @Test
-    @DisplayName("Позитивные проверка поиска по релизам в репозитории junit-team/junit4 по номеру")
+    @Owner("xenia-efimova")
+    @DisplayName("Позитивная проверка поиска по релизам в репозитории junit-team/junit4 по номеру")
+    @Feature("Страница с релизами в репозитории")
     public void shouldSearchCorrectlyByNumberTest() {
-        // Кликнуть на заголовок Releases
-        TestPages.repositoryPage.titleReleases().click();
-        // Проверка отображения поисковой строки
-        TestPages.repositoryPage.releasesSearchString().shouldBe(Condition.visible);
-        //Поиск по номеру
-        TestPages.repositoryPage.releasesSearchString().sendKeys("4.13.2");
-        // Нажать Enter
-        TestPages.repositoryPage.releasesSearchString().pressEnter();
-        //Проверка существования названия релиза с введенным номером
-        TestPages.repositoryPage.releaseCards().shouldHave(size(1));
-        TestPages.repositoryPage.releaseCards().shouldHave(texts("4.13.2"));
+        step("Кликнуть на заголовок Releases", () -> {
+            TestPages.repositoryPage.titleReleases().click();
+        });
+        step("Проверка отображения поисковой строки", () -> {
+            TestPages.repositoryPage.releasesSearchString().shouldBe(Condition.visible);
+        });
+        step("Поиск по номеру", () -> {
+            TestPages.repositoryPage.releasesSearchString().sendKeys("4.13.2");
+        });
+        step("Нажать Enter", () -> {
+            TestPages.repositoryPage.releasesSearchString().pressEnter();
+        });
+        step("Проверка существования названия релиза с введенным номером", () -> {
+            TestPages.repositoryPage.releaseCards().shouldHave(size(1));
+            TestPages.repositoryPage.releaseCards().shouldHave(texts("4.13.2"));
+        });
     }
 
     @Test
-    @DisplayName("Позитивные проверка поиска по релизам в репозитории junit-team/junit4 по буквам в названии")
+    @Owner("xenia-efimova")
+    @DisplayName("Позитивная проверка поиска по релизам в репозитории junit-team/junit4 по буквам в названии")
+    @Feature("Страница с релизами в репозитории")
     public void shouldSearchCorrectlyByLettersTest() {
-        // Кликнуть на заголовок Releases
-        TestPages.repositoryPage.titleReleases().click();
-        // Проверка отображения поисковой строки
-        TestPages.repositoryPage.releasesSearchString().shouldBe(Condition.visible);
-        //Поиск по буквам в названии
-        TestPages.repositoryPage.releasesSearchString().sendKeys("Beta");
-        TestPages.repositoryPage.releasesSearchString().pressEnter();
-        //Проверка существования названия релиза с введенными буквами
-        TestPages.repositoryPage.releaseCards().shouldHave(size(6));
-        TestPages.repositoryPage.releaseCards().first().shouldHave(text("Beta"));
+        step("Кликнуть на заголовок Releases", () -> {
+            TestPages.repositoryPage.titleReleases().click();
+        });
+        step("Проверка отображения поисковой строки", () -> {
+            TestPages.repositoryPage.releasesSearchString().shouldBe(Condition.visible);
+        });
+        step("Поиск по буквам в названии", () -> {
+            TestPages.repositoryPage.releasesSearchString().sendKeys("Beta");
+            TestPages.repositoryPage.releasesSearchString().pressEnter();
+        });
+        step("Проверка существования названия релиза с введенными буквами", () -> {
+            TestPages.repositoryPage.releaseCards().shouldHave(size(6));
+            TestPages.repositoryPage.releaseCards().first().shouldHave(text("Beta"));
+        });
     }
 }
